90f0741861b3dce51853a144c9b262137accf48d
C0mplexity0 <106925362+C0mplexity0@users.noreply.github.com>
2026-04-14 19:04:18 +0100

    fix: Update glob paths on server

--- places/main/src/server/main.server.ts
+++ places/main/src/server/main.server.ts
@@ -5,7 +5,7 @@ import { makeHello } from "shared/module";
 print(makeHello("main.server.ts"));
 print(makeHelloFromCommon("main.server.ts"));
 
-Flamework.addPaths("places/main/src/server/helloService.ts");
-Flamework.addPaths("places/common/src/server/helloCommonService.ts");
+Flamework.addPathsGlob("places/main/src/server/**/*");
+Flamework.addPathsGlob("places/common/src/server/**/*");
 
 Flamework.ignite();
